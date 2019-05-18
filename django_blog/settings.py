c4c21ecdee43fdcfef938c48c794263c667b0765
Rolandas <Rolandas1369>
2019-05-18 10:38:09 +0300

    templates dir

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/Rolandas1369/rolandas1369.pythonanywhere.com/django_blog/templates/'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
